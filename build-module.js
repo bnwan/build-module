848638f7122103e96b02e3a2eb7e6ed6b8dcd2e9
bnwan <benjamin@weareadaptive.com>
2015-01-29 12:00:15 +0000

    initial commit

---
+++ build-module.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var b = require('browserify')();
+var fs = require('fs');
+
+var api = {
+	bundle: function(options){
+		b.add(options.root);
+		b.bundle(function (err, code) {
+		    fs.writeFileSync(options.name + '.bundle.js', code);
+		});
+	}
+};
+
+module.exports = {
+	bundle: function(options){
+		api.bundle(options);
+	}
+};
---
+++ build.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var build = require('./build-module');
+build.bundle({
+	name: 'build-module',
+	root: './build-module.js'
+});
