5beb9dcf304ff9d711f10ab8f7ca2f7573de1874
bnwan <benjamin@weareadaptive.com>
2015-03-26 13:02:19 +0000

    updated with extra options

--- build-module.js
+++ build-module.js
@@ -4,28 +4,31 @@ var browserify = require('browserify');
 var fs = require('fs');
 
 var api = {
-	bundle: function (options) {
-		if (!options) {
-			options = {};
-		}			
-	
-		var b = browserify(options.root, options.bundleOptions);
+  bundle: function (options) {
+    if (!options) {
+      options = {};
+    }
 
-		if (Array.isArray(options.transforms)) {
-			options.transforms.forEach(function (t) {				
-				b.transform({
-          global: true
+    var b = browserify(options.root, options.bundleOptions);
+
+    if (Array.isArray(options.transforms)) {
+      options.transforms.forEach(function (t) {
+        b.transform({
+          sourcemap: options.bundleOptions.debug,
+          compress: options.bundleOptions.debug,
+          mangle: options.bundleOptions.debug,
+          global: options.bundleOptions.debug
         }, t);
-			});
-		}
-		
-		b.bundle().pipe(fs.createWriteStream(options.output));
-	}
+      });
+    }
+
+    b.bundle().pipe(fs.createWriteStream(options.output));
+  }
 };
 
 
 module.exports = {
-	bundle: function (options) {
-		api.bundle(options);
-	}
+  bundle: function (options) {
+    api.bundle(options);
+  }
 };
